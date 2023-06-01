85cc18efa17fc670c9a802b5878cdae49b95fb42
Mohamadhosein Shabaniraad <mh.shabanirad@icloud.com>
2023-06-01 16:09:13 +0330

    add Parent class Product

---
+++ src/main/java/ir/ac/kntu/models/Product.java
@@ -0,0 +1,69 @@
+package ir.ac.kntu.models;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public abstract class Product {
+
+    private String name;
+
+    private String details;
+
+    private double price;
+
+    private ProductType productType;
+
+    private ArrayList<Community> communities;
+
+    public Product(String name, String details, double price, ProductType productType) {
+        this.name = name;
+        this.details = details;
+        this.price = price;
+        this.productType = productType;
+        communities = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public ArrayList<Community> getCommunities() {
+        return communities;
+    }
+
+    public void setCommunities(ArrayList<Community> communities) {
+        this.communities = communities;
+    }
+
+    public void showAllComment(){
+        for (Community community : communities){
+            community.showComment();
+        }
+    }
+
+    public void addCommunity(Community community){
+        communities.add(community);
+    }
+
+
+}
